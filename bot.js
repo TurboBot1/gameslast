35d6b0832c2b6c44914d04bc7c8d6e02a672ce85
ii7mody12 <44589893+ii7mody12@users.noreply.github.com>
2019-04-11 22:36:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ client.on('ready', () => {
   console.log('')
   console.log('')
   console.log('')
-  client.user.setActivity('#Games')
+  client.user.setActivity('$Games')
 });
 
 client.on('message', message => {
